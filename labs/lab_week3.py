4cc4191e555fc38185569937b66677fda10cb54e
micheal cannon <cannonmicheal@hotmail.com>
2020-10-14 22:52:20 +0100

    completed parts 1-3 of lab_week3

---
+++ labs/lab_week3.py
@@ -0,0 +1,10 @@
+# Write a program that stores the data for all trains in Ireland in a csv file
+
+
+import requests
+import csv
+from bs4 import BeautifulSoup
+url = "http://api.irishrail.ie/realtime/realtime.asmx/getCurrentTrainsXML"
+page = requests.get(url)
+soup = BeautifulSoup(page.content, 'xml')
+# print (soup.prettify())
