1bbaa84ab7dc310a0bfceef9ceea07e8fba6a54b
micheal cannon <cannonmicheal@hotmail.com>
2020-10-23 20:45:59 +0100

    added first lab of week 4

--- labs/lab_week3.py
+++ labs/lab_week3.py
@@ -4,6 +4,15 @@
 import requests
 import csv
 from bs4 import BeautifulSoup
+retrieveTags=['TrainStatus',
+'TrainLatitude',
+'TrainLongitude',
+'TrainCode',
+'TrainDate',
+'PublicMessage',
+'Direction'
+]
+
 url = "http://api.irishrail.ie/realtime/realtime.asmx/getCurrentTrainsXML"
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'xml')
@@ -13,10 +22,13 @@ with open('week03_train.csv', mode='w') as train_file:
     train_writer = csv.writer(train_file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     listings = soup.findAll("objTrainPositions")
     for listing in listings:
+            lat =float( listing.TrainLatitude.string)
+            if (lat < 53.4):        
 # print(listing)
-        entryList = []
-# print(listing.TrainLatitude.string)
-# or
-        entryList.append(print(listing.find('TrainLatitude').string))
-        train_writer.writerow(entryList)
+                entryList = []
+                for retrieveTag in retrieveTags:
+
+        #print (listing.find(retrieveTag).string)
+                         entryList.append(print(listing.find(retrieveTag).string))
+                train_writer.writerow(entryList)
 
