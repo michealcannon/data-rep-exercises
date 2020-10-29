b3cc478c65f65cb330b7163c40f1bb813890eca0
micheal cannon <cannonmicheal@hotmail.com>
2020-10-29 23:05:23 +0000

    added code to serve static pages

--- labs/week 05/server/simpleserver.py
+++ labs/week 05/server/simpleserver.py
@@ -1,7 +1,13 @@
 # simple app server with flask that serves hello world
 
 from flask import Flask
-app = Flask(__name__)
+
+# Get the app server to be able to serve static pages
+app = Flask(__name__,   
+ static_url_path='',
+ static_folder='../')
+
+
 @app.route('/')
 def index():
  return "Hello, World!"
